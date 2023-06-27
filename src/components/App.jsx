b528a95199adde3ccbf09e594373c4c5e5d733e0
YuriyMikh <pamukkale1717@gmail.com>
2023-06-28 02:50:24 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { AppContainer } from './App.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import { fetchPixabay } from './services/api-pixabay';
